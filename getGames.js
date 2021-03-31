8cc90d96bc3af109130786737728bc8ea6109e8a
Jack Cooper <jack@acooper.net>
2021-03-31 14:58:32 +0100

    CommaRemove

--- getGames.js
+++ getGames.js
@@ -9,6 +9,11 @@ var config = {
   
   axios(config)
   .then(function (response) {
+  //  var data = JSON.stringify(response.data);
+    var parse = response.data.applist.apps;
+    for (gameIdx in response.data.applist.apps){
+      response.data.applist.apps[gameIdx].name =  response.data.applist.apps[gameIdx].name.replace(/,/g,' ');
+    }
     var data = JSON.stringify(response.data);
     fs.writeFile('./games.json', data, 'utf8');
     console.log('Games Written to File')
