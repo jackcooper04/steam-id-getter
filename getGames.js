88f46c2a1be53b9673a59bfbfcd29c8464d5e7a3
Jack Cooper <jack@acooper.net>
2021-03-31 15:12:43 +0100

    erroradd

--- getGames.js
+++ getGames.js
@@ -1,7 +1,8 @@
 const fs = require('fs-extra');
 const axios = require('axios');
 console.log('Getting Games from Steam')
-var config = {
+function getGames(){
+  var config = {
     method: 'get',
     url: 'http://api.steampowered.com/ISteamApps/GetAppList/v0002/',
     headers: { }
@@ -10,6 +11,11 @@ var config = {
   axios(config)
   .then(function (response) {
   //  var data = JSON.stringify(response.data);
+  if (response.data.applist.apps.length == 0){
+    console.log('Error: Retrying');
+    getGames();
+    return;
+  }
     var parse = response.data.applist.apps;
     for (gameIdx in response.data.applist.apps){
       response.data.applist.apps[gameIdx].name =  response.data.applist.apps[gameIdx].name.replace(/,/g,' ');
@@ -21,3 +27,5 @@ var config = {
   .catch(function (error) {
     console.log(error);
   });
+}
+getGames();
